12327e41867cb1049869095028acc37224d7dcb0
Dom Maciulaitis <dommaciulaitis@kubrickgroup.com>
2024-03-06 10:06:21 +0000

    Working

--- etl_pipeline_shared.py
+++ etl_pipeline_shared.py
@@ -9,7 +9,7 @@ table_column_mapping = {
     'StgActivityLog':{'User_logs_cleaned.csv':['ActivityID','UserID','date','login_time','logout_time']},
     'StgJobTitle':{'cleaned.csv':['JobID','job_title']},
     'StgLocation':{'cleaned.csv':['LocationID','city','country']},
-    'StgPaymentCard':{'cleaned.csv':['card_number','card_type','subscription']},
+    'StgPaymentCard':{'cleaned.csv':['card_type','subscription', 'card_number']},
     'StgUser':{'cleaned.csv':['UserID','username','first_name','last_name','email','gender','password','phone_number','birth_date','account_creation_date','profile_picture_URL','ip_address']},
 }
 
@@ -29,7 +29,13 @@ def load_data_to_sql():
         for csv_name, columns in csv_column_mapping.items():
             df_data = pd.read_csv(csv_name)
             selected_columns = [col for col in columns if col in df_data.columns]
-            if selected_columns:
+            if selected_columns and table_name == "StgPaymentCard":
+                df_selected = df_data[selected_columns]
+                # Card number cant be null in sql table
+                df_selected = df_selected.dropna(subset=["card_number"])
+                df_selected.to_sql(table_name, engine, if_exists='append', index=False)
+                print(f'Data loaded into table {table_name} from csv {csv_name}')
+            elif selected_columns:
                 df_selected = df_data[selected_columns]
                 df_selected.to_sql(table_name, engine, if_exists='append', index=False)
                 print(f'Data loaded into table {table_name} from csv {csv_name}')
