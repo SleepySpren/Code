04d29dfd433c328f15c69c65a7e4080c1abb82fc
Dom Maciulaitis <dommaciulaitis@kubrickgroup.com>
2024-03-06 09:48:57 +0000

    slight alteration to get it to use csv files

--- etl_pipeline_shared.py
+++ etl_pipeline_shared.py
@@ -4,17 +4,13 @@ import pypyodbc
 import pandas as pd
 from variables import SERVER_NAME, DATABASE_NAME, USERNAME, PASSWORD
 
-
-excel_file = '' # Add you cleaned excel file location 
-
-table_sheet_column_mapping = {
-    # '<SQLtablename>' :{'<Excel sheet name>':['<Column names>'] }   
-    # 'StgActivityLog':{'Usage Logs':['ActivityID','UserID','date','login_time','logout_time']},
-    # 'StgInterest':{'List of Interest Topics':['InterestID','Topic']},
-    'StgJobTitle':{'Accounts':['JobID','job_title']},
-    'StgLocation':{'Accounts':['LocationID','city','country']},
-    'StgPaymentCard':{'Accounts':['card_number','card_type','subscription']},
-    'StgUser':{'Accounts':['CardNumber','username','first_name','last_name','email','gender','password','phone_number','birth_date','account_creation_date','profile_picture_URL','ip_address']},
+table_column_mapping = {
+    # '<SQLtablename>' :{'<csv file>':['<Column names>'] }   
+    'StgActivityLog':{'User_logs_cleaned.csv':['ActivityID','UserID','date','login_time','logout_time']},
+    'StgJobTitle':{'cleaned.csv':['JobID','job_title']},
+    'StgLocation':{'cleaned.csv':['LocationID','city','country']},
+    'StgPaymentCard':{'cleaned.csv':['card_number','card_type','subscription']},
+    'StgUser':{'cleaned.csv':['UserID','username','first_name','last_name','email','gender','password','phone_number','birth_date','account_creation_date','profile_picture_URL','ip_address']},
 }
 
 def load_data_to_sql():
@@ -29,16 +25,16 @@ def load_data_to_sql():
     engine = create_engine(connection_url, module=pypyodbc)
 
 
-    for table_name, sheet_column_mapping in table_sheet_column_mapping.items():
-        for sheet_name, columns in sheet_column_mapping.items():
-            df_data = pd.read_excel(excel_file, sheet_name=sheet_name)
+    for table_name, csv_column_mapping in table_column_mapping.items():
+        for csv_name, columns in csv_column_mapping.items():
+            df_data = pd.read_csv(csv_name)
             selected_columns = [col for col in columns if col in df_data.columns]
             if selected_columns:
                 df_selected = df_data[selected_columns]
                 df_selected.to_sql(table_name, engine, if_exists='append', index=False)
-                print(f'Data loaded into table {table_name} from sheet {sheet_name}')
+                print(f'Data loaded into table {table_name} from csv {csv_name}')
             else:
-                print(f'No columns specified for table {table_name} in sheet {sheet_name}')
+                print(f'No columns specified for table {table_name} in csv {csv_name}')
 
 if __name__ =="__main__":
     load_data_to_sql()
